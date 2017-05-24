9f146f5d4f6fbd9e77786e50ae9824ad18002350
udit01 <uditjainiitd@gmail.com>
2017-05-25 02:26:30 +0530

    unleashed many admin features search list filter title edit fields edit etc

---
+++ src/posts/admin.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import Post
+
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ["title","updated","timestamp"]
+    list_display_links = ["updated"]
+    list_editable = ["title"]
+    list_filter = ["updated","timestamp"]
+    search_fields = ["title", "content"]
+    class Meta:
+        model = Post
+
+admin.site.register(Post,PostModelAdmin)
+
+
+
